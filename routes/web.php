03d09799d149b019d38768a4823700268e765a2a
Abrar Ahmad <abrar.dev99@gmail.com>
2023-01-09 12:28:47 +0500

    wip

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::middleware([
+    'universal',
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
